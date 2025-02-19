8ad9b510d41732b65ce7262edd514625e68d0623
Nguyen Hieu <nguyenhieuxyz159@gmail.com>
2025-02-19 09:51:08 +0700

    [ADD] Application for Attending Courses Menu

--- custom_addons/hino_training/models/application/application_for_ATC.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class ApplicationForATC(models.Model):
-    _name = 'application'
-    _description = 'Application for Attending Training Course'
-
-    name = fields.Char(string='Name')
