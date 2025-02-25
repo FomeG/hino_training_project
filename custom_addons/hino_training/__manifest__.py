18fa9768fe3840f097980a5d7dcf0dd8708be853
Dương Tú <duong0968031143@gmail.com>
2025-02-25 15:23:36 +0700

    [IMP] Search View and List View

--- custom_addons/hino_training/__manifest__.py
+++ custom_addons/hino_training/__manifest__.py
@@ -35,10 +35,9 @@ Long description of module's purpose
         'views/hmv_training_brochure.xml',
       
         'views/hmv_training_courses_form_view.xml',
-        'views/training_course_report.xml',
         'views/test.xml',
 
-        # 'views/action.xml',
+        'views/action.xml',
         'views/trainning_plan_view.xml',
 
         'templates/application_report_template.xml',
