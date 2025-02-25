100f6e03349f795e97d3e842823b5e65bfd76718
Lê Chính Đại <140992360+RelliZisme98@users.noreply.github.com>
2025-02-25 15:22:02 +0700

    [ADD/FIX] Fix Tab Approval history and ADD template report

--- custom_addons/hino_training/__manifest__.py
+++ custom_addons/hino_training/__manifest__.py
@@ -35,9 +35,10 @@ Long description of module's purpose
         'views/hmv_training_brochure.xml',
       
         'views/hmv_training_courses_form_view.xml',
+        'views/training_course_report.xml',
         'views/test.xml',
 
-        'views/action.xml',
+        # 'views/action.xml',
         'views/trainning_plan_view.xml',
 
         'templates/application_report_template.xml',
