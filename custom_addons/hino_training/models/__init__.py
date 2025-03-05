9fa5a07dc8ec9e36edca6ec7668d2bb3ec65fe33
Hai-anh2722003 <nguyenduchaianh03@gmail.com>
2025-03-05 09:53:29 +0700

    [FIX] training plan and reorder files

--- custom_addons/hino_training/models/__init__.py
+++ custom_addons/hino_training/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import hmv_training_plan,models,hmv_list_value_line
-from . import models
+
 from . import hmv_training_need
 from . import hmv_training_need_line
 
@@ -10,7 +10,7 @@ from . import hmv_training_course_approval
 from . import hmv_training_participant
 from . import hmv_training_brochure
 from . import hmv_training_brochure_line
-from . import hmv_training_brochure_wizard
+
 
 from . import hmv_list_value_line
 
